6fe4e3b7817b6a49f2922ec1f305f124fbeb8e98
asselqi <34541972+asselqi@users.noreply.github.com>
2020-10-19 14:44:35 +0300

    Making code asynchronous

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,7 +1,6 @@
-namespace API.Entities
-{
-    public class AppUser
-    {
-        
+namespace API.Entities {
+    public class AppUser {
+        public int Id { set; get; }
+        public string UserName { get; set; }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -2,57 +2,49 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace API
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
+namespace API {
+    public class Startup {
+        private readonly IConfiguration _config;
+
+        public Startup (IConfiguration config) {
+            this._config = config;
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddDbContext<DataContext> (options => {
+                options.UseSqlite (_config.GetConnectionString ("DefaultConnection"));
             });
+            services.AddSwaggerGen(_config => {
+                _config.SwaggerDoc("v1", new OpenApiInfo {Title = "SnowBlaze1.API", Version = "v1"});
+            });
+            services.AddControllers ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+                app.UseSwagger ();
+                app.UseSwaggerUI (c => c.SwaggerEndpoint ("/swagger/v1/swagger.json", "API v1"));
             }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseHttpsRedirection ();
+            app.UseRouting ();
+            app.UseAuthorization ();
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
             });
         }
     }
